ab732a2f929356a22a22f6da663cb86c9c075fcc
ratchetwrench <david.r.wrench@gmail.com>
2017-12-02 15:08:24 -0700

    move api code to app.py flask code

---
+++ data.py
@@ -0,0 +1,23 @@
+USERS = [{
+    'bio': 'Data Science, Python, and Statistics.',
+    'login': 'ratchetwrench',
+    'repos_url': 'https://api.github.com/users/ratchetwrench/repos',
+    'url': 'https://api.github.com/users/ratchetwrench'
+},
+    {
+        'bio': 'Web Dev, Javascript, yayyyy',
+        'login': 'rahul4times',
+        'repos_url': 'https://api.github.com/users/rahul4times/repos',
+        'url': 'https://api.github.com/users/rahul4times'
+    },
+    {
+        'bio': 'Sup.',
+        'login': 'chrisb01',
+        'repos_url': 'https://api.github.com/users/chrisb01/repos',
+        'url': 'https://api.github.com/users/chrisb01'
+    }
+]
+
+TAGS = ["100daysofcode", "python", "java", "LA", "pycon2018", "dockercon2018"]
+
+Languages = ["Python", "Javascript", "Java", "C#", "Go", "C++"]
